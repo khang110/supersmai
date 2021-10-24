ede2c99ceec013d5e22382113aa1ff9d4ce34d9e
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-10-24 18:27:19 +0700

    add folder

---
+++ src/assets/fonts/Icon.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AntDesign,
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Fontisto,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
+} from "@expo/vector-icons";
+
+export const Icons = {
+  AntDesign,
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Fontisto,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
+};
+
+const Icon = ({ type, name, color, size = 24, style }) => {
+  const fontSize = 24;
+  const Tag = type;
+  return (
+    <>
+      {type && name && (
+        <Tag name={name} size={size || fontSize} color={color} style={style} />
+      )}
+    </>
+  );
+};
+
+export default Icon;
